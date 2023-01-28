ca5bb972a17b236ce8d16c48b634691a6adaf4dd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-28 13:43:49 -0500

    changed flexbox to grid

--- pages/buildings/index.tsx
+++ pages/buildings/index.tsx
@@ -15,7 +15,7 @@ export default function index() {
           <p className="max-w-3xl text-gray-600 body-large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis possimus harum voluptates. Consectetur cumque itaque repudiandae sint aspernatur laborum expedita.</p>
         </section>
         <Tabs />
-        <section className="flex flex-col gap-8 mt-8">
+        <section className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-3">
           {properties.map((data) => {
             return <PropertyCard key={data.id} Headline={data.title} Path={data.link} Meida={data.image} />
           })}
