9211802a45c3068f6674a9d41eec9b563c003d8b
Justin Bento <bento.a.justin@gmail.com>
2023-01-31 23:50:00 -0500

    filter tab gorup based on status

--- pages/buildings/index.tsx
+++ pages/buildings/index.tsx
@@ -1,7 +1,8 @@
-  import Head from "next/head"
+import Head from "next/head"
 import PropertyCard from "../../components/PropertyCard"
-import Tabs from "../../components/Tabs"
 import { properties } from "../../lib/data"
+import { Tab } from '@headlessui/react'
+
 
 export default function index() {
   return (
@@ -14,13 +15,39 @@ export default function index() {
           <h1 className="mb-2 headline-large">Current Buildings</h1>
           <p className="max-w-3xl text-gray-600 body-large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis possimus harum voluptates. Consectetur cumque itaque repudiandae sint aspernatur laborum expedita.</p>
         </section>
-        <Tabs />
-        <section className="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-3">
-          {properties.map((data) => {
-            return <PropertyCard key={data.id} Headline={data.title} Path={data.link} Meida={data.image} />
-          })}
-          
+        <section className="">
+          <Tab.Group>
+            <Tab.List className="space-x-4">
+              <Tab>Buying</Tab>
+              <Tab>Selling</Tab>
+              <Tab>Renting</Tab>
+            </Tab.List>
+            <Tab.Panels as="section">
+              <Tab.Panel>
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                  {properties.filter(person => person.status == "Buying").map(filteredPerson => (
+                    <PropertyCard key={filteredPerson.id} Headline={filteredPerson.title} Meida={filteredPerson.image} Path={filteredPerson.title.toString().toLowerCase().replace(/\s+/g, '-')} Status="Buying" />
+                  ))}
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                  {properties.filter(person => person.status == "Selling").map(filteredPerson => (
+                    <PropertyCard key={filteredPerson.id} Headline={filteredPerson.title} Meida={filteredPerson.image} Path={filteredPerson.title.toString().toLowerCase().replace(/\s+/g, '-')} Status="Selling" />
+                  ))}
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                  {properties.filter(person => person.status == "Renting").map(filteredPerson => (
+                    <PropertyCard key={filteredPerson.id} Headline={filteredPerson.title} Meida={filteredPerson.image} Path={filteredPerson.title.toString().toLowerCase().replace(/\s+/g, '-')} Status="Renting" />
+                  ))}
+                </div>
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
         </section>
+
       </main>
     </>
   )
