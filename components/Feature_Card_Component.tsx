68b590915aaacb92f0f09591444880c3d17ccd44
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-20 14:49:27 -0500

    created a component for comapny features

---
+++ components/Feature_Card_Component.tsx
@@ -0,0 +1,15 @@
+import { MdOutlineInsertLink } from "react-icons/md";
+
+export default function Feature_Card_Component() {
+  return (
+    <>
+    <div className="border rounded overflow-hidden border-[#191C1D]">
+      <div className="px-4 py-5 space-y-1.5 sm:p-6">
+        <MdOutlineInsertLink className="w-6 h-6" />
+        <h2 className="font-bold">Experiential Properties</h2>
+        <p className="">Temporary and permanent branded spaces.</p>
+      </div>
+    </div>
+  </>
+  )
+}
