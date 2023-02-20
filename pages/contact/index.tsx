03997e0c56ae9fc0101f029f7d7d5a490d5cbd8d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-20 15:58:06 -0500

    updatedd contact form for using react-hook-form

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useForm, SubmitHandler } from "react-hook-form";
 import { HiPhone, HiMail } from 'react-icons/hi'
 import Apbbar from '../../components/Apbbar'
 import Footer from '../../components/Footer'
@@ -9,7 +10,7 @@ export default function Contact() {
       <Head>
         <title>Paramount Propertiies - Contact</title>
       </Head>
-      <Apbbar/>
+      <Apbbar />
       <div className="relative bg-white">
         <div className="absolute inset-0">
           <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
@@ -49,73 +50,38 @@ export default function Contact() {
           </div>
           <div className="px-6 py-16 bg-white lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
             <div className="max-w-lg mx-auto lg:max-w-none">
-              <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
-                <div>
-                  <label htmlFor="full-name" className="sr-only">
-                    Full name
-                  </label>
-                  <input
-                    type="text"
-                    name="full-name"
-                    id="full-name"
-                    autoComplete="name"
-                    className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500"
-                    placeholder="Full name"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="email" className="sr-only">
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500"
-                    placeholder="Email"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="phone" className="sr-only">
-                    Phone
-                  </label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    autoComplete="tel"
-                    className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500"
-                    placeholder="Phone"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="message" className="sr-only">
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    rows={4}
-                    className="block w-full px-4 py-3 placeholder-gray-500 border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500"
-                    placeholder="Message"
-                    defaultValue={''}
-                  />
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center px-6 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+              <Email_Form />
             </div>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   )
+}
+
+type Inputs = {
+  full_name: string,
+  email: string,
+  email_subject: string,
+  email_message: string,
+};
+
+
+function Email_Form() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+      {/* register your input into the hook by invoking the "register" function */}
+      <input placeholder="Full Name" {...register("full_name")} className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500" />
+      <input placeholder="Email" {...register("email")} className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500" />
+      <input placeholder="Subject" {...register("email_subject")} className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500" />
+      <textarea placeholder="Message" {...register("email_message")} rows={4} className="block w-full px-4 py-3 placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:border-primary-500 focus:ring-primary-500" />
+      {errors.email_message && <span>This field is required</span>}
+      <input type="submit" className="inline-flex justify-center px-6 py-3 text-base font-medium text-white border border-transparent rounded-md shadow-sm placeholder-slate-400 bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2" />
+    </form>
+  );
 }
