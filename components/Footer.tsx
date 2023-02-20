8f9c8ef219f676d0d5a419b72379e16769babadc
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-02-20 15:20:05 -0500

    included a form submission for newsletter subscription

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
 const navigation = {
   solutions: [
     { name: 'Marketing', href: '#' },
@@ -155,28 +157,7 @@ export default function Example() {
             <p className="mt-2 text-sm leading-6 text-gray-300">
               The latest news, articles, and resources, sent to your inbox weekly.
             </p>
-            <form className="mt-6 sm:flex sm:max-w-md">
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email-address"
-                id="email-address"
-                autoComplete="email"
-                required
-                className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
-                placeholder="Enter your email"
-              />
-              <div className="mt-4 sm:mt-0 sm:ml-4 sm:flex-shrink-0">
-                <button
-                  type="submit"
-                  className="flex items-center justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-sky-500 hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
-                >
-                  Subscribe
-                </button>
-              </div>
-            </form>
+            <Newsletter_Form />
           </div>
         </div>
         <div className="pt-8 mt-16 border-t border-white/10 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
@@ -194,5 +175,30 @@ export default function Example() {
         </div>
       </div>
     </footer>
+  )
+}
+
+type Inputs = {
+  email: string,
+};
+
+function Newsletter_Form() {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => alert(data);
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-6 sm:flex sm:max-w-md">
+        {/* include validation with required or other standard HTML validation rules */}
+        <div className="flex flex-col">
+          <input {...register("email", { required: true })} placeholder="Enter your email" className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" />
+          {/* errors will return when field validation fails  */}
+          {errors.email && <span className="text-xs text-white">This field is required</span>}
+        </div>
+        <div className="mt-4 sm:mt-0 sm:ml-4 sm:flex-shrink-0">
+          <input type="submit" className="flex items-center justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-sky-500 hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500" />
+        </div>
+      </form>
+      );
+    </>
   )
 }
